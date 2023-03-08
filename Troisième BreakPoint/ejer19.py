56920a2ce6dba8eb36f5327dcb83d04b90f4d81f
AgustinGomezDelToro <agustinngomezdeltoro@gmail.com>
2023-03-08 20:12:22 +0100

    ejer19 partie 3

---
+++ Troisième BreakPoint/ejer19.py
@@ -0,0 +1,9 @@
+# Initialisation de la liste vide truc
+truc = []
+
+# Initialisation de la liste machin avec cinq flottants nuls
+machin = [0.0] * 5
+
+# Affichage des deux listes
+print("truc :", truc)
+print("machin :", machin)
