413b4d0d31e123958fcc93533d2492ac6865e9b6
AgustinGomezDelToro <agustinngomezdeltoro@gmail.com>
2023-03-08 20:12:34 +0100

    ejer20 partie 3

---
+++ Troisième BreakPoint/ejer20.py
@@ -0,0 +1,27 @@
+
+for i in range(4):
+    print(i)
+
+# Affichage des entiers de 4 à 7
+for i in range(4, 8):
+    print(i)
+
+# Affichage des entiers de 2 à 8 par pas de 2
+for i in range(2, 9, 2):
+    print(i)
+
+
+
+# Définition de la liste chose
+chaine = [0, 1, 2, 3, 4, 5]
+
+# Test de l'appartenance des éléments 3 et 6 à chose
+if 3 in chaine:
+    print("3 est présent dans la chainee")
+else:
+    print("3 n'est pas présent dans la chaine")
+
+if 6 in chaine:
+    print("6 est présent dans la chaine")
+else:
+    print("6 n'est pas présent dans la chaine")
