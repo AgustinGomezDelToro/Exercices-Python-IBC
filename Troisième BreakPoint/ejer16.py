e9eef14594325ba06765ea2f8832f42d2475da16
AgustinGomezDelToro <agustinngomezdeltoro@gmail.com>
2023-03-08 20:11:24 +0100

    ejer16 partie 3

---
+++ Troisième BreakPoint/ejer16.py
@@ -0,0 +1,8 @@
+# Saisie de la chaîne
+chaine = input("Entrez une chaîne : ")
+
+# Inversion de la chaîne
+inverse = chaine[::-1]
+
+# Affichage de l'inverse
+print("L'inverse de la chaîne est :", inverse)
